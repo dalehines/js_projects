32e4572496450e4669f52c59d27252b505ed0251
dalehines <dahines87@gmail.com>
2023-04-03 00:23:20 -0500

    almost fully responsive. menu icon clicks functional

--- portfolio_website/portfolio_site/script.js
+++ portfolio_website/portfolio_site/script.js
@@ -4,3 +4,23 @@ var typed = new Typed('.multi-text', {
     loop:true
 
 })
+
+/* toggle menu icon */
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+
+}
+
+window.onscroll = () => {
+    /* remove menu icon and toggle icon when clicked  */
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+}
+
+ 
