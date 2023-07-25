57ac3790d63359ff5430f857313bed3b5bdaba75
dalehines <dahines87@gmail.com>
2023-07-24 23:45:48 -0500

    menu + social icons added

--- portfolio_website/Portfolio_website_2/assets/js/main.js
+++ portfolio_website/Portfolio_website_2/assets/js/main.js
@@ -14,3 +14,13 @@ const closeMenu = document.getElementById('nav-close')
     closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
+
+// REMOVE MENU AFTER ICON CLICK
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
