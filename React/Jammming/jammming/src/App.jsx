9284ba54b70dfcd2e16e5a0d5b111e7d2127170f
dalehines <dahines87@gmail.com>
2023-08-20 09:27:15 -0500

    from scratch

---
+++ React/Jammming/jammming/src/App.jsx
@@ -0,0 +1,22 @@
+/* Create Static Components
+Create components for your Jammming application. You may structure your components as you see fit, but you should have a component representing each of these core components of the interface:
+
+App
+SearchBar
+SearchResults
+Playlist
+Tracklist
+Track
+Additionally, make sure that your interface has a Save To Spotify button and a Search button.
+ */
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
