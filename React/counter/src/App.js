4caa47373d6d5732ca70d02f1b25cefaa27da301
dalehines <dahines87@gmail.com>
2023-10-23 22:46:48 -0500

    counter

---
+++ React/counter/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+
+
+
+  // const handleClick = (e) =>{
+  //   e.target.value
+  // }
+
+
+
+  return (
+    <div>
+        
+          
+          <div className='container'>
+          <p className='heading'>Lets start counting!</p>
+          <div className=''></div>
+        
+          <button className='button'>-</button>
+          <button className='button'>+</button>
+          </div>
+      
+    </div>
+  );
+}
+
+export default App;
