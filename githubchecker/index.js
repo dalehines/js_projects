6e4e5b0f6b05e82125cca4c2543a4111d6ccf723
dalehines <dahines87@gmail.com>
2023-10-29 23:24:25 -0500

    async function written

---
+++ githubchecker/index.js
@@ -0,0 +1,14 @@
+const searchGithub = async () => {
+    const username = document.getElementById("searchInput").value;
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const detailsContainer = document.querySelector(".details");
+    const data = await response.json();
+
+
+
+
+
+
+
+
+}
