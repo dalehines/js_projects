be2efcb2542fa46eaebd4815e8e2df70b3a191d4
dalehines <dahines87@gmail.com>
2024-01-10 00:07:46 -0500

    first of 2024

--- Navbar/assets/js/main.js
+++ Navbar/assets/js/main.js
@@ -8,7 +8,10 @@ const navMenu = document.getElementById('nav-menu'),
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
-
+// menu hide
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
+
+    let movieName = movieNameRef.value;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
